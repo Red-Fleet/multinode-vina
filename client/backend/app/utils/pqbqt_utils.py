6352a455762b6763dda9a00188a578c8860d8b81
anshul yadav <anshul145263@gmail.com>
2023-04-03 12:45:08 +0530

    fixed pdbqt parsing function of client

--- client/backend/app/utils/pqbqt_utils.py
+++ client/backend/app/utils/pqbqt_utils.py
@@ -11,11 +11,27 @@ class PdbqtUtils:
         Returns:
             list[str]: list of str contaning ligands
         """
-        ligands = re.split(r'\n *ENDMDL', pdbqt)
+        # splitting pdbqt file in lines
+        lines = pdbqt.split("\n")
+        ligands = [] # for storing parsed ligand
+        curr_ligand = ""
         
-        # only add ENDMDL if it was removed
-        if bool(re.split(r'\n *ENDMDL', pdbqt)) == True:
-            ligands = [ligand+'\nENDMDL' for ligand in ligands]
+        for line in lines:
+            if "MODEL" == line[0:5]:
+                # start of new ligand file
+                curr_ligand = ""
+            elif "ENDMDL" == line[0:6]:
+                # end of current ligand, save it, if not empty
+                if curr_ligand != "":
+                    ligands.append(curr_ligand)
+                    curr_ligand = ""
+            else:
+                # save line in current ligand
+                curr_ligand += line + "\n"
+
+        # if curr_ligand is not empty then save
+        if curr_ligand != "":
+            ligands.append(curr_ligand) 
 
         return ligands
         
